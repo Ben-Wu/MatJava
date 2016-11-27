ffea94daa673c8789cef040e3edb5ac5383aa4a7
Ben Wu <bw96412@hotmail.com>
2016-11-27 00:07:42 -0500

    multi threaded compression for better performance, added comments to compression code

--- matjava2/src/main/java/ca/benwu/examples/ImageCompressionExample.java
+++ matjava2/src/main/java/ca/benwu/examples/ImageCompressionExample.java
@@ -13,67 +13,97 @@ import ca.benwu.Matrix;
 
 public class ImageCompressionExample {
 
+    private static int numberOfColours = 0;
+
+    private static long startTime;
+
+    private static int counter = 0;
+
     public static void main(String[] args) {
         try {
             Scanner s = new Scanner(System.in);
             System.out.println("Input filename: ");
 
-            String inputFilename = "scene.jpg";
+            String inputFilename = "house.jpg";
 
             System.out.println("Output filename: ");
 
-            String outputFilename = "compressed.png";
+            String outputFilename = "compressed.jpg";
 
             // read image pixel data
-            int[][][] pixelData = ImageProcessor.readImage(inputFilename);
+            final int[][][] pixelData = ImageProcessor.readImage(inputFilename);
+
+            numberOfColours = pixelData.length;
 
             final int BLOCK_SIZE = 16;
             final int COMPRESSED_BLOCK_SIZE = 8;
 
-            int[][][] compressedPixelData = new int[pixelData.length][][];
+            final int[][][] compressedPixelData = new int[pixelData.length][][];
+
+            startTime = System.currentTimeMillis();
+
+            System.out.print("Compressing...");
 
             // fourier transform for each BLOCK_SIZE by BLOCK_SIZE block in each colour matrix
             for(int i = 0 ; i < pixelData.length ; i++) {
-                System.out.println("Colour " + (i + 1));
-                // create matrix representing pixel data for current colour
-                Matrix colourMatrix = new Matrix(pixelData[i]);
-                // matrix for pixel data after compression
-                Matrix compressedMatrix = null;
-                System.out.print("Compressing...");
-                for(int row = 0 ; row + BLOCK_SIZE <= colourMatrix.getHeight() ; row += BLOCK_SIZE) {
-                    System.out.print(".");
-                    Matrix compressedRow = null;
-                    for(int col = 0 ; col + BLOCK_SIZE <= colourMatrix.getWidth() ; col += BLOCK_SIZE) {
-                        Matrix compressedBlock = Fourier.dct(colourMatrix.subMatrix(row, col, row + BLOCK_SIZE - 1, col + BLOCK_SIZE - 1))
-                                .subMatrix(0,0,COMPRESSED_BLOCK_SIZE-1,COMPRESSED_BLOCK_SIZE-1);
+                final int colourIndex = i;
+                // multi threading for much better performance
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // create matrix representing pixel data for current colour
+                        Matrix colourMatrix = new Matrix(pixelData[colourIndex]);
+                        // matrix for pixel data after compression
+                        Matrix compressedMatrix = null;
+                        // run algorithm on each block
+                        for(int row = 0 ; row + BLOCK_SIZE <= colourMatrix.getHeight() ; row += BLOCK_SIZE) {
+                            System.out.print(".");
+                            Matrix compressedRow = null;
+                            for(int col = 0 ; col + BLOCK_SIZE <= colourMatrix.getWidth() ; col += BLOCK_SIZE) {
+                                // perform dct and take only the first COMPRESSED_BLOCK_SIZE coeffiecients
+                                Matrix compressedBlock = Fourier.dct(colourMatrix.subMatrix(row, col, row + BLOCK_SIZE - 1, col + BLOCK_SIZE - 1))
+                                        .subMatrix(0,0,COMPRESSED_BLOCK_SIZE-1,COMPRESSED_BLOCK_SIZE-1);
 
-                        compressedBlock = compressedBlock.horzcat(Matrix.zeros(COMPRESSED_BLOCK_SIZE,BLOCK_SIZE-COMPRESSED_BLOCK_SIZE))
-                                .vertcat(Matrix.zeros(BLOCK_SIZE-COMPRESSED_BLOCK_SIZE,BLOCK_SIZE));
+                                // zero-pad sub-matrix
+                                compressedBlock = compressedBlock.horzcat(Matrix.zeros(COMPRESSED_BLOCK_SIZE,BLOCK_SIZE-COMPRESSED_BLOCK_SIZE))
+                                        .vertcat(Matrix.zeros(BLOCK_SIZE-COMPRESSED_BLOCK_SIZE,BLOCK_SIZE));
 
-                        compressedBlock = Fourier.idct(compressedBlock);
+                                // convert back to regular pixel array
+                                compressedBlock = Fourier.idct(compressedBlock);
 
-                        if(compressedRow == null) {
-                            compressedRow = compressedBlock;
-                        } else {
-                            compressedRow = compressedRow.horzcat(compressedBlock);
+                                // add compressed block to row
+                                if(compressedRow == null) {
+                                    compressedRow = compressedBlock;
+                                } else {
+                                    compressedRow = compressedRow.horzcat(compressedBlock);
+                                }
+                            }
+                            // add compressed row to matrix
+                            if(compressedMatrix == null) {
+                                compressedMatrix = compressedRow;
+                            } else {
+                                compressedMatrix = compressedMatrix.vertcat(compressedRow);
+                            }
                         }
+                        compressedPixelData[colourIndex] = compressedMatrix.round();
+                        counter++;
                     }
-                    if(compressedMatrix == null) {
-                        compressedMatrix = compressedRow;
-                    } else {
-                        compressedMatrix = compressedMatrix.vertcat(compressedRow);
-                    }
+                }).start();
+            }
+
+            while(counter < numberOfColours - 1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+
                 }
-                System.out.println();
-                compressedPixelData[i] = compressedMatrix.round();
             }
 
             ImageProcessor.writeImage(compressedPixelData, outputFilename);
 
-            System.out.print("done");
+            System.out.print("done: " + (System.currentTimeMillis() - startTime));
         } catch (IOException e) {
             System.out.print("failed");
         }
     }
-
 }
